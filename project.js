1ddc4404a0f6994a1780c1c6aff0653a82b2d948
DilnazBolatovna <dilnaz.bolatovna03@gmail.com>
2023-03-18 22:02:15 +0600

    midterm

---
+++ project.js
@@ -0,0 +1,54 @@
+
+function readMore(){
+    var dots = document.getElementById("dots");
+    var more = document.getElementById("more");
+    var btn = document.getElementById("btn");
+
+
+
+if(dots.style.display === "none"){
+    dots.style.display="inline";
+    btn.innerHTML="+";
+    more.style.display="none";
+}else{
+    dots.style.display="none";
+    btn.innerHTML="X";
+    more.style.display="inline";
+}
+
+
+}
+function readMoree(){
+    var dots = document.getElementById("dotss");
+    var more = document.getElementById("moree");
+    var btn = document.getElementById("btn1");
+
+
+if(dots.style.display === "none"){
+    dots.style.display="inline";
+    btn.innerHTML="+";
+    more.style.display="none";
+}else{
+    dots.style.display="none";
+    btn.innerHTML="X";
+    more.style.display="inline";
+}
+
+}
+function readMoreee(){
+    var dots = document.getElementById("dotsss");
+    var more = document.getElementById("moreee");
+    var btn = document.getElementById("btn2");
+
+
+if(dots.style.display === "none"){
+    dots.style.display="inline";
+    btn.innerHTML="+";
+    more.style.display="none";
+}else{
+    dots.style.display="none";
+    btn.innerHTML="X";
+    more.style.display="inline";
+}
+
+}
